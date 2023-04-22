daae3f1fcb295f987ed369415d4186434bcdb634
Rachad Bouhjar <s131576@ap.be>
2023-04-22 07:57:35 +0200

    code wordt opgefrist en mongoDb branch aangemaakt

--- index.ts
+++ index.ts
@@ -6,26 +6,35 @@ app.set('view engine', 'ejs');
 app.set('port', 4000);
 app.use(express.json({limit:"2mb"}));
 app.use(express.urlencoded({ extended: true }));
+interface Avatar {
+  name: string;
+  description: string;
+  type: string;
+  rarity: string;
+  series: string;
+  images: {
+    icon: string;
+  };
+}
 let apiImages:any= [];
-let apiWapons:any= []; // deze variabele wordt later gedeclareerd
-let apiBackpack:any= []; // deze variabele wordt later gedeclareerd
+let apiWapons:any= [];
+let apiBackpack:any= [];
 let blacklist:any = [];
-let favorietAvatars:any=[]; // deze variabele wordt later gedeclareerd
+let favorietAvatars:any=[];
 app.get('/', (req, res) => {
   res.render("landingpage");
 });
 
 app.get('/fortniteHome', async (req, res) => {
-  // Een GET-request uitvoeren naar een externe API om gegevens over Fornite items op te halen
   let fortniteResponse = await axios.get("https://fortnite-api.theapinetwork.com/items/list");
-  let record = fortniteResponse.data; //hier wordt de data in record variabele gestoken
-  let avatars= []; // hier declaren we dat array variable 'avatars' leeg is
+  let record = fortniteResponse.data; 
+  let avatars= []; 
   try {
     for (let i = 0; i < record.data.length; i++) {
-      let random = Math.floor(Math.random() * 1000); //Hier zoeken we een random getal tussen 0 en 1000
-      let item = record.data[random].item; // in de data object in the api zit namelijk een item object => dat stellen we gelijk met de random
-      if (item.type === "outfit") { // in de item.type zitten verschillende soorten 'teksten' zoals outfit,backpack etc.. daarom filteren
-        let avatar = { //hier zorgen we ervoor dat we object aanmaken met de gegevens die wij willen
+      let random = Math.floor(Math.random() * 1000); 
+      let item = record.data[random].item; 
+      if (item.type === "outfit") {
+        let avatar :Avatar= {
           name: record.data[random].item.name,
           description: record.data[random].item.description,
           type: record.data[random].item.type,
@@ -33,50 +42,42 @@ app.get('/fortniteHome', async (req, res) => {
           series: record.data[random].item.series,
           images: record.data[random].item.images.icon
         };
-        avatars.push(avatar);//hier zorgen we ervoor dat we de item die we aanmaken wordt gepushed
-        apiImages.push(avatar);// Waarom er hier 2 pushes staan, is omdat wanneer er refreshed worden er telkens andere random object(avatar) staan
+        avatars.push(avatar);
+        apiImages.push(avatar);
       }
     }
   }
-  catch (error) { //als er een error voorkomt we naar de error.ejs file wordt gerenderd
+  catch (error) {
     console.log(error);
   }
   res.render("fortniteHome", {
-    avatarImage: avatars  //hier stellen we vast dat object avatars op de forniteHome kan opgeroepen worden door avatarImage
+    avatarImage: avatars
   });
 });
-let favoriteImages:any = []; // we maken eerst een lege array variabele met de naam favoriteImages
-app.post('/favoriet', (req, res) => { //Hier zorgen we wanneer er een form post wordt doorgestuurd opgevangen word met de action pad /favoriet
-  let name = req.body.name; //Hier vangen we name die wordt doorgestuurd uit de post request. Name is namelijk de name die we vaststellen in de form name.
-  let image = req.body.image; //Hier wordt hetzelfde gedaan
-// Hier zoeken we de index in de apiImages of de image die we opvangen hetzelfde is dat van de apiImages.images
+let favoriteImages:any = [];
+app.post('/favoriet', (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
   let apiImageIndex = apiImages.findIndex((apiImage:any) => apiImage.images === image);
-  
-  /*Deze code controleert of apiImageIndex groter is dan of gelijk is aan 0. Als dat waar is, wijst het id toe aan de waarde van id in apiImages bij de index van apiImageIndex. Als het niet waar is, wijst het id toe aan de waarde van -1. Met andere woorden, als er een geldige index is, krijgt id de id-waarde van de afbeelding in apiImages, anders krijgt id de waarde -1 toegewezen.*/
   let id = apiImageIndex >= 0 ? apiImages[apiImageIndex].id : -1;
   
-  // Deze code stuur een object door naar de array die boven de post request hebben gedeclareerd
   favoriteImages.push({ name: name, image: image, id: id });
   
-  res.redirect('/fortniteHome'); // redirect naar forniteHome  wanneer je klikt op de button (send)
+  res.redirect('/fortniteHome'); 
 });
 app.get("/favoriet", async (req, res) => {
-//deze code haalt de id uit de parameter, en omgezet naar een int. Als er geen id parameter is ingesteld wordt het automatisch op -1 gezet. Als die id kleiner dan 0 of groter is dan de array die we bij de post request voor dit pad hebben gepushed: Krijg je een error => zodat er geen undifined of null krijgen.
   const id = parseInt(req.query.id?.toString() ?? "-1");
-  //the req.query.id is uit de post request form ==> het is hidden dus je zal het niet zien in de url.Wanneer het formulier wordt ingediend via een POST-request, wordt wel meegestuurd met het formulier als het wordt verzonden. Daarom we gebruiken van req.query.id?.toString() om die verstopte id eruit te halen
   if (id < 0 || id >= favoriteImages.length) {
     return res.render("error");
   }
-  const favoriteImage = favoriteImages[id]; // deze code zoeken we de id dit we doorgestuure in form /favoriet
-  
-  // find the corresponding apiImages object
-  let apiImage = apiImages.find((apiImage:any) => apiImage.id === favoriteImage.id);//Deze code zoekt het eerste object in de apiImages array waarvan de id eigenschap gelijk is aan de id eigenschap van het favoriteImage object.
-  let apiBackpack = [];//Deze code zorgt ervoor dat we een lege array hebben voordat we er gegevens in gaan zetten
-  let apiWapons = [];//Deze code zorgt ervoor dat we een lege array hebben voordat we er gegevens in gaan zetten
+  const favoriteImage = favoriteImages[id];
+  let apiImage = apiImages.find((apiImage:any) => apiImage.id === favoriteImage.id);
+  let apiBackpack = [];
+  let apiWapons = [];
   const fortniteResponse = await axios.get("https://fortnite-api.theapinetwork.com/items/list");
   const record = fortniteResponse.data;
   for (let i = 0; i <= record.data.length; i++) {
-    const random = Math.floor(Math.random() * record.data.length); // Deze code zorgt voor een random getal tussen 0 en de data die we fetche
+    const random = Math.floor(Math.random() * record.data.length);
     if (record.data[random].item.type === "backpack") {
       apiBackpack.push(record.data[random]);
     }
@@ -85,21 +86,21 @@ app.get("/favoriet", async (req, res) => {
     }
   }
   res.render("fortniteChar", {
-    character: favoriteImage,//deze code is de we op lijn 70 hebben opgezocht in de array op id
-    apiImage: apiImage, // deze code is dus de item die we over mappen dezelfde id hebben op lijn 73 
+    character: favoriteImage,
+    apiImage: apiImage,
     avatarBackpack: apiBackpack,
     avatarPickaxe: apiWapons,
     info:apiImages
   });
 });
-app.get('/favoriet-images', async (req, res) => { //deze code is omdat we andere te veel get requesten doen naar dezelfde route.Dus /favoriete is voor informatie uit te halen voor post/req. En reden we naar forniteChar.
+app.get('/favoriet-images', async (req, res) => { 
   const fortniteResponse = await axios.get("https://fortnite-api.theapinetwork.com/items/list");
   const record = fortniteResponse.data;
   let apiBackpack = [];
   let apiWapons = [];
   for (let i = 0; i <= record.data.length; i++) {
     const random = Math.floor(Math.random() * record.data.length); 
-    if (record.data[random].item.type === "backpack") {// Deze code doen we opnieuw omdat we een andere pagina renderen. Dit kan nog geweizigd worden zodat we backpack uit /fornite route halen
+    if (record.data[random].item.type === "backpack") {
       apiBackpack.push(record.data[random]);
     }
     if (record.data[random].item.type === "pickaxe") {
@@ -107,21 +108,16 @@ app.get('/favoriet-images', async (req, res) => { //deze code is omdat we andere
     }
   }
   res.render('favoriet', { 
-    favoriteImages: favoriteImages, //Deze code is al gedeclareerd met waardes in the get request/favorite
+    favoriteImages: favoriteImages,
     apiWapons: apiWapons, 
     apiBackpack: apiBackpack 
   });
 });
-
-
 app.post('/blacklist', (req, res) => {
-  const { id, blacklistReason, image } = req.body; /*Deze code is hetzelfde als: 
-  const id = req.body.id;
-  const blacklistReason = req.body.blacklistReason;
-  const image = req.body.image;*/
-  const imageObj = apiImages[id]; //Deze code zet imageObj=apiImages[id =>id die we uit de post req halen]
+  const { id, blacklistReason, image } = req.body;
+  const imageObj = apiImages[id]; 
   if (imageObj) {
-    blacklist.unshift({ name: imageObj.name, images: image, blacklistReason }); // blacklist is een lege array die we pushen met dat object.
+    blacklist.unshift({ name: imageObj.name, images: image, blacklistReason }); 
   }
   res.redirect('fortniteHome');
 });
@@ -130,11 +126,9 @@ app.post('/blacklist/:id', (req, res) => {
   const { _method } = req.body;
 
   if (_method === 'DELETE') {
-    // delete the item with the specified id from the blacklist
     blacklist.splice(id, 1);
     res.redirect('/blacklist');
   } else {
-    // handle other POST requests
     const { blacklistReason } = req.body;
     blacklist[id].blacklistReason = blacklistReason;
     res.redirect('/blacklist');
